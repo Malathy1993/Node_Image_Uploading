07d99a5a795c78c3d8b817d49be883cc223cb731
jerome11sgic <jerome11sgic@gmail.com>
2020-04-30 19:38:00 +0530

    Image uploading in ejs with multer

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const ejs = require('ejs')
+const multer = require('multer')
+const path = require('path')
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+    destination : './public/uploads',
+    filename : function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Init Upload
+const upload = multer({
+    storage : storage,
+    limits : {fileSize : 1000000},
+    fileFilter : function(req, file, cb){
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname){
+        return cb(null, true);
+    }else{
+        cb('Error : Images Only!');
+    }
+}
+
+// init app
+const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+// public Folder
+app.use(express.static('./public'));
+
+app.get('/',(req,res) => {
+    res.render('index',{
+        title : 'Node File Uploads'
+    })
+})
+
+app.post('/upload',(req,res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('index',{
+                title : 'Node File Uploads',
+                msg : err
+            })
+        }else{
+            if(req.file == undefined){
+                res.render('index',{
+                    msg : 'Error : No File Selected!',
+                    title : 'Node File Uploads',
+                });
+            }else{
+                res.render('index',{
+                    msg : 'File Uploaded',
+                    title : 'Node File Uploads',
+                    file : `uploads/${req.file.filename}`
+                });
+            }
+        }
+    })
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+    
+})
